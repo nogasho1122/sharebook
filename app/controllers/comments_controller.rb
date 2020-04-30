6c6efe33981f476148132bc052734569548ba2e2
nogasho1122 <exile1122sho@icloud.com>
2020-05-01 07:51:54 +0900

    T

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,6 @@ class CommentsController < ApplicationController
     end
   end
 
-
   private 
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
