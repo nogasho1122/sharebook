b3f1757bc429be164f47ee2fc766a950822c41fe
nogasho1122 <exile1122sho@icloud.com>
2020-05-29 20:02:11 +0900

    UT

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,7 +3,6 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  
   def self.search(search)
     return Tweet.all  unless search
     Tweet.where('text LIKE(?)', "%#{search}%")
